4164ab5976b27e0a325ad2ebffca025ec9f59413
Alex Friberg <classay@gmail.com>
2014-11-04 22:25:39 -0500

    more tweaking

--- app/views/simplenode.blade.php
+++ app/views/simplenode.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>simplenode</title>
-    <script type="text/javascript" src="http://localhost:8333/socket.io/socket.io.js"></script>
+    <!--<script type="text/javascript" src="http://localhost:8333/socket.io/socket.io.js"></script>-->
     <script type="text/javascript" src='{{ asset('scripts/client.js') }}'></script>
 </head>
 <body>
